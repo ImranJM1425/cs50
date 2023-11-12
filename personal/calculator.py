a15efe7a957a32d5f923d1bb03037a3e339af798
Imran <imransg86@gmail.com>
2023-11-13 01:38:16 +0800

    added

---
+++ personal/calculator.py
@@ -0,0 +1,22 @@
+#x = int(input("what is x? "))
+#y = int(input("what is y? "))
+
+#print(x+y)
+
+#Accept decimal numbers
+#x = float(input("what is x? "))
+#y = float(input("what is y? "))
+
+#Round the answer
+#z = round(x+y)
+
+#Separate numbers triple digits
+#print(f"{z:,}")
+
+x = float(input("what is x? "))
+y = float(input("what is y? "))
+
+z= x + y
+
+#Prints only 2 decimal points
+print(f"{z:.2f}")
---
+++ personal/hello.py
@@ -0,0 +1,15 @@
+# # Ask user for name
+# name = input("what's your name? ").strip().title()
+
+# #Split first and last name
+# first, last = name.split(" ")
+# # Say hello to user
+# print (f"hello, {first}")
+
+#Defining hello. To create my own functions
+def hello(to="world"):
+    print("Hello,", to)
+
+hello()
+name = input("What's your name? ").strip().title()
+hello(name)
